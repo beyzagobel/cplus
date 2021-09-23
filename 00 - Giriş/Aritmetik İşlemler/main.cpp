821d6d0243d33fa74f3ab86280d07582996bcb1e
byzgbl <byzgbll@gmail.com>
2021-09-24 01:23:20 +0300

    new

---
+++ 00 - Giriş/Aritmetik İşlemler/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout<<8+2;
+
+    cout<<endl;
+
+    cout<<8-2;
+
+    cout<<endl;
+
+    cout<<8*2;
+
+    cout<<endl;
+
+    cout<<8/2;
+
+
+}
