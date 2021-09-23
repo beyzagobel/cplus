57ab29e4c93b3d4d624c6967ad932ae31625c13d
byzgbl <byzgbll@gmail.com>
2021-09-23 23:09:45 +0300

    new

--- 00 - Giriş/2- Çıktı Yazdırma (cout)/örnek2.cpp
+++ 00 - Giriş/2- Çıktı Yazdırma (cout)/örnek2.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main(){
 
-    string ad="Beyza";     // değişken tanımlamaayı sonra anlatacağım şimdilik çıktıya odaklanalım :)
+    string ad="Beyza";  
+    int yas=22;             // değişken tanımlamaayı sonra anlatacağım şimdilik çıktıya odaklanalım :)
     cout<<"Merhaba, benim adim "<<ad<<" memnun oldum.";
+
+    cout<<endl;
+
+    cout<<"Ben "<<yas<<" yasindayim.";
+
+    return 0;
 
 /* Output
 
 Merhaba, benim adim Beyza memnun oldum.
+Ben 22 yasindayim.
 
 */
 }
