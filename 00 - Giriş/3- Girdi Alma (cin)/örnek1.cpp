c226228d112b5c6c20ecc21b7d6ccb1e073f40b9
byzgbl <byzgbll@gmail.com>
2021-09-24 00:48:11 +0300

    new

--- 00 - Giriş/3- Girdi Alma (cin)/örnek1.cpp
+++ 00 - Giriş/3- Girdi Alma (cin)/örnek1.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main(){
-    
-    int
+ 
+    int yil;
+    cout<<"Hangi yilda dogdunuz :";
+    cin>>yil;
+    cout<<"Yasiniz :"<<2021-yil;
+    return 0;
+
+/*
+ Output
+
+Hangi yilda dogdunuz :1999  // burada ben kendi doğum yılımı yazdım ve enter a bastım
+Yasiniz :22      // burada ise program biz hesaplama yaparak bana yaşımın çıktısını vermiş oldu
+
+*/  
+
 }
