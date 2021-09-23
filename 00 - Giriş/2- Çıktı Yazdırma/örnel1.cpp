b09d795fc6a92d6e3a62cfde766fc8a2c63e117f
byzgbl <byzgbll@gmail.com>
2021-09-23 21:21:44 +0300

    new

---
+++ 00 - Giriş/2- Çıktı Yazdırma/örnel1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"4 + 5 = ";
+    cout<<4+5;
+    // bu iki satır kod yerine aşağıdaki tek satır kodda yeterli olucaktır //
+    cout<<"4 + 5 = "<<4+5;
+}
