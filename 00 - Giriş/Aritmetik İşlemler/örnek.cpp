7dca8250706741111be716919aae9efc5e4ab61e
byzgbl <byzgbll@gmail.com>
2021-09-24 01:28:42 +0300

    new

--- 00 - Giriş/Aritmetik İşlemler/örnek.cpp
+++ 00 - Giriş/Aritmetik İşlemler/örnek.cpp
@@ -3,20 +3,7 @@ using namespace std;
 
 int main(){
     
-    cout<<"beyza"
-   // cout<<8+2;
-
-   // cout<<endl;
-
- /*   cout<<8-2;
-
-    cout<<endl;
-
-    cout<<8*2;
-
-    cout<<endl;
-
+  
     cout<<8/2;
-*/
 
 }
