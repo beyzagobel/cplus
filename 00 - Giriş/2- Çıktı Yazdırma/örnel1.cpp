56128a5886b0476f7d434b7f05d8e06e32225107
byzgbl <byzgbll@gmail.com>
2021-09-23 21:40:39 +0300

    new

--- 00 - Giriş/2- Çıktı Yazdırma/örnel1.cpp
+++ 00 - Giriş/2- Çıktı Yazdırma/örnel1.cpp
@@ -5,5 +5,6 @@ int main(){
     cout<<"4 + 5 = ";
     cout<<4+5;
     // bu iki satır kod yerine aşağıdaki tek satır kodda yeterli olucaktır //
+    cout<<endl;       // endl komutu bir satır atlatır.
     cout<<"4 + 5 = "<<4+5;
 }
