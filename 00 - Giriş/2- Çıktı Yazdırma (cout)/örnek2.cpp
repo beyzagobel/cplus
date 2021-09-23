91700ec9f134f5825969a7cf7a010948e8631fc9
byzgbl <byzgbll@gmail.com>
2021-09-23 23:04:08 +0300

    new

---
+++ 00 - Giriş/2- Çıktı Yazdırma (cout)/örnek2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string ad="Beyza";     // değişken tanımlamaayı sonra anlatacağım şimdilik çıktıya odaklanalım :)
+    cout<<"Merhaba, benim adim "<<ad<<" memnun oldum.";
+}
