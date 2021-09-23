d24b862d5bd485a663c32bf06d20e43b49635c31
byzgbl <byzgbll@gmail.com>
2021-09-23 22:52:12 +0300

    new

--- 00 - Giriş/2- Çıktı Yazdırma/örnek1.cpp
+++ 00 - Giriş/2- Çıktı Yazdırma/örnek1.cpp
@@ -2,15 +2,32 @@
 using namespace std;
 
 int main(){
+
     cout<<"4 + 5 = ";
     cout<<4+5;
-    // bu iki satır kod yerine aşağıdaki tek satır kodda yeterli olucaktır //
+    
     cout<<endl;       // endl komutu bir satır atlatır.
+
     cout<<"4 + 5 = "<<4+5;
+
+    cout<<endl;
+
+    cout<<"4 + 5"<<endl<<"="<<endl<<4+5; 
+
+    cout<<endl;
+
+    cout<<"4"<<" + "<<"5"<<" = "<<4+5;
+   
+    // burada olduğu gibi nasıl çıktı almak istiyorsanız veya nasıl yazmak isterseniz o şekilde kodlarınızı düzenleyebilirsiniz 
+
 }
 
-"""
+/*"""
 Output:
 4 + 5 = 9
 4 + 5 = 9
-"""
+4 + 5
+=
+9
+4 + 5 = 9
+"""*/
