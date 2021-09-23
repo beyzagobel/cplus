11367dc7e6af339e66c859a23b222544e5695e5b
byzgbl <byzgbll@gmail.com>
2021-09-23 22:38:40 +0300

    new

---
+++ 00 - Giriş/2- Çıktı Yazdırma/örnek1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"4 + 5 = ";
+    cout<<4+5;
+    // bu iki satır kod yerine aşağıdaki tek satır kodda yeterli olucaktır //
+    cout<<endl;       // endl komutu bir satır atlatır.
+    cout<<"4 + 5 = "<<4+5;
+}
+
+"""
+Output:
+4 + 5 = 9
+4 + 5 = 9
+"""
