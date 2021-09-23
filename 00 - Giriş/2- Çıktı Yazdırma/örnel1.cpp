5adb4adf669af810c2ad8dddd65f39cf0803e4f3
Beyza Göbel <78444522+byzgbl@users.noreply.github.com>
2021-09-23 21:31:17 +0300

    Update örnel1.cpp

--- 00 - Giriş/2- Çıktı Yazdırma/örnel1.cpp
+++ 00 - Giriş/2- Çıktı Yazdırma/örnel1.cpp
@@ -5,8 +5,13 @@ int main(){
     cout<<"4 + 5 = ";
     cout<<4+5;
     // bu iki satır kod yerine aşağıdaki tek satır kodda yeterli olucaktır //
+    cout<<endl;       // endl komutu bir satır atlatır.
     cout<<"4 + 5 = "<<4+5;
 }
 
-çıktı
+"""
+Output:
+
 4 + 5 = 9
+
+"""
