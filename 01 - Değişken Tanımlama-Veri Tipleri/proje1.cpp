37287a328ca785dbf094a8d110d65d81058ca138
byzgbl <byzgbll@gmail.com>
2021-09-28 00:00:56 +0300

    new

---
+++ 01 - Değişken Tanımlama-Veri Tipleri/proje1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int birinci_degisken;      //bu iki paragraf yerine de tek satırdada değişken tanımlayabiliriz
+    int ikinci_degisken;
+    
+    cout<<"Birinci degiskeni giriniz : ";
+    cin>>birinci_degisken;
+    cout<<"Ikinci degiskeni giriniz : ";
+    cin>>ikinci_degisken;
+    cout<<"Birinci Degisken + I5kinci Degisken = "<<birinci_degisken+ikinci_degisken;
+
+    return 0;
+
+/*
+Output
+
+Birinci degiskeni giriniz : 3
+Ikinci degiskeni giriniz : 7
+Birinci Degisken + Ikinci Degisken = 10
+
+*/
+
+}
