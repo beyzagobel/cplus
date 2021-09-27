bf15290a1cebf1fdd39aa61c8764b01bbb474141
byzgbl <byzgbll@gmail.com>
2021-09-28 01:44:53 +0300

    new

--- 01 - Değişken Tanımlama-Veri Tipleri/proje2.cpp
+++ 01 - Değişken Tanımlama-Veri Tipleri/proje2.cpp
@@ -8,7 +8,7 @@ int main(){
     string ad,soyad;
     int kg;
     float boy;
-    float vucut_kitle_indeksi=kg/(boy*boy); 
+    float beden_kitle_indeksi=kg/(boy*boy); 
 
     cout<<"Ad - Soyad : ";
     cin>>ad>>soyad;
@@ -17,8 +17,17 @@ int main(){
     cin>>kg;
     cout<<"Boyunuzu giriniz : ";
     cin>>boy;
-    cout<<ad<<" "<<soyad<<" 'in vucut kitle indeksi : "<<vucut_kitle_indeksi;
+    cout<<ad<<" "<<soyad<<" 'in beden kitle indeksi : "<<beden_kitle_indeksi;
     return 0;
 
+/*
+
+Output :
+
+Ad - Soyad : Beyza Gobel
+Merhaba Beyza Gobel
+Vucut agirliginizi giriniz : 53
+Boyunuzu giriniz : 1.60
+Beyza Gobel 'in beden kitle indeksi : 20.7
 
 }
