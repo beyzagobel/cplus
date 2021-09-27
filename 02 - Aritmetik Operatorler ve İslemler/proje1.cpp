e59309ba2c3a17de6cf23be0f1bcb54d5845b064
byzgbl <byzgbll@gmail.com>
2021-09-28 00:49:30 +0300

    new

---
+++ 02 - Aritmetik Operatorler ve İslemler/proje1.cpp
@@ -0,0 +1,30 @@
+// Kullanıcıdan iki tane sayı alan ve bu iki sayı ile dört işlem yapan program
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int sayi1,sayi2;
+    cout<<"Birinci sayiyi giriniz : ";
+    cin>>sayi1;
+    cout<<"İkinci sayiyi giriniz : ";
+    cin>>sayi2;
+
+// Toplama İşlemi
+   cout<<sayi1<<" + "<<sayi2<<" = "<<sayi1+sayi2;
+
+// Çıkarma İşlemi
+
+   cout<<sayi1<<" - "<<sayi2<<" = "<<sayi1-sayi2;
+
+// Çarpma İşlemi
+
+   cout<<sayi1<<" * "<<sayi2<<" = "<<sayi1*sayi2;
+
+// Bölme İşlemi
+
+   cout<<sayi1<<" / "<<sayi2<<" = "<<sayi1/sayi2;
+
+    return 0;
+}
