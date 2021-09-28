7d2cea0dd544bae373d100ae839fa875d064d038
byzgbl <byzgbll@gmail.com>
2021-09-28 23:48:57 +0300

    new

--- 02 - Operatörler/2- Aritmetik Atama Operatörleri/atama_ornekleri.cpp
+++ 02 - Operatörler/2- Aritmetik Atama Operatörleri/atama_ornekleri.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-    
+
+    int sayi1,sayi2;
+
+    cout<<"sayi1 = ";
+    cin>>sayi1;
+    cout<<"sayi2 = ";
+    cin>>sayi2;
+
+    sayi1+=sayi2;    // sayi1 ve sayi2'yi topla değerini sayi1'e ata
+
+    cout<<"sayi+=sayi2 isleminin degeri = "<<sayi1<<endl;
+
+    return 0;
+
 }
