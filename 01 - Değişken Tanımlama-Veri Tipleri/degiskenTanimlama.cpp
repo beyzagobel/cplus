51ac3d5495c20dcca54be98c15386ffb2f9ab819
byzgbl <byzgbll@gmail.com>
2021-09-28 18:56:08 +0300

    new

--- 01 - Değişken Tanımlama-Veri Tipleri/degiskenTanimlama.cpp
+++ 01 - Değişken Tanımlama-Veri Tipleri/degiskenTanimlama.cpp
@@ -3,20 +3,23 @@ using namespace std;
 
 int main(){
 
-    int yas;        //yas ismindeki tamsayı(int) tipindeki değişken
+    int yas;         //yas ismindeki tamsayı(int) tipindeki değişken
 
     float toplam;    //toplam ismindeki ondalıklı sayı(float) tipindeki değişken
 
-    string ad;      //ad ismindeki string(metin ögesi) tipindeki değişken
+    string ad;       //ad ismindeki string(metin ögesi) tipindeki değişken
 
 // yukarıda 3 farklı değişken tanımlama yapılmış fakat bu değişkenlere herhangi bir değer(value) verilmemiş.
 // aşağıdaki gibi değişkenler tanımlanırken ilk atamalarıda yapılabilir.
 
-   int yas=21; // yas değişkenine 21 atanmış.
+   int yas=21;            // yas değişkenine 21 atanmış.
 
-   float toplam=3.25; // toplam değişkenine 3.25 değeri atanmış.
+   float toplam=3.25;     // toplam değişkenine 3.25 değeri atanmış.
 
-   string ad="Beyza"; // ad değişkenine Beyza değeri atanmış.
+   string ad="Beyza";    // ad değişkenine Beyza değeri atanmış.
+
+   bool a=true;          // ya da    
+   bool b=5>3;           // 5, 3'ten büyük ise true, değilse false atanır
 
 // çoklu değişken tanımlarken;
 
@@ -26,19 +29,21 @@ int main(){
 
 // ya da;
 
-   int sayi1,sayi2,sayi3;  // üstteki 3 paragrafı bu şekilde tek paragraftada tanımlayabiliriz
+   int sayi1,sayi2,sayi3;    // üstteki 3 paragrafı bu şekilde tek paragraftada tanımlayabiliriz
 
 // çoklu değişkenlerin ilk atamsı yapılırken;
 
    int sayi1=5;
    int sayi2=7;
    int sayi3=3;
+
 // ya da;
+
    int sayi1=5,sayi2=7,sayi3=3;   // yine üstteki 3 paragraf yerine tek satırda değişkenlerin ilk ataması yapılabilir
 
 //ya da;                              
    
-   int yeni_sayi=sayi1;  // diyerekte sayi1 değişkeninin değerini yeni_sayi değişkenine atamış oluruz
+   int yeni_sayi=sayi1;         // diyerekte sayi1 değişkeninin değerini yeni_sayi değişkenine atamış oluruz
 
    int toplam=sayi1+sayi1+sayi3;    // değişkenleri aritmetik işlemlere tabi tutup sonuçlarınıda bir değişkene atayabiliriz
 
