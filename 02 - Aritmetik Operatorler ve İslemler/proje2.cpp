a56915dd6913116d7873537837b3f9855101257c
byzgbl <byzgbll@gmail.com>
2021-09-28 18:41:42 +0300

    new

--- 02 - Aritmetik Operatorler ve İslemler/proje2.cpp
+++ 02 - Aritmetik Operatorler ve İslemler/proje2.cpp
@@ -4,14 +4,19 @@ using  namespace std;
 int main(){
 
 // Kullanıcıdan girilen bir karenin kenarından alan ve çevre hesabının yapılması
+
     int kare_kenari;
     cout<<"Karenin kenarini giriniz : ";
     cin>>kare_kenari;
-    cout<<"Karenin cevresi : "<<kare_kenari*4<<endl;
-    cout<<"Karenin alani : "<<kare_kenari*kare_kenari<<endl;
+// program sırayla okunduğu için önce kullanıcıdan karenin kenarının girilmesi beklenir, sonra alan ve cevre hesabı yapılarak sonuç
+// alan ve cevre değişkenine atanır, buraya dikkat ediniz !!
+    int cevre=kare_kenari*4;
+    int alan=kare_kenari*kare_kenari;
+    cout<<"Karenin cevresi : "<<cevre<<endl;
+    cout<<"Karenin alani : "<<alan<<endl;
 
 // Kullanıcıdan girilen dikdörtgen kenarları ile alan ve çevre hesabının yapılması
-
+// dikdörtgenin alan ve çevre sonucunu tutan bir değişken tanımlanmamış direk hesaplama yapılmış, bu şekildede yazılabilir
    int uzun_kenar,kisa_kenar;
    cout<<"Dikdortgenin uzun kenarini giriniz : ";
    cin>>uzun_kenar;
