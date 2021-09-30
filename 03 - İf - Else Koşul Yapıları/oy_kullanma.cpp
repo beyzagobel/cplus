cd23bffdb7c8540899aad26ceb3cacaad16d6820
byzgbl <byzgbll@gmail.com>
2021-09-30 21:49:56 +0300

    new

---
+++ 03 - İf - Else Koşul Yapıları/oy_kullanma.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// Yaşınıza göre oy kullanıp kullanamadğınızı ekrana yazan program
+
+    int yas;
+    cout<<"Yasinizi giriniz : ";
+    cin>>yas;
+    if(yas>=18){
+         cout<<"Oy kullananabilirsiniz."<<endl;
+    }
+    else{
+        cout<<"Oy kullanamazsiniz."<<endl;
+    }
+    cout<<"Hoscakalin";
+    return 0;
+
+}
